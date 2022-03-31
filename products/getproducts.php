4ad73adea3cae6071112b9d518e36fcf77898f7b
Annala <n1anje00@students.oamk.fi>
2022-03-31 13:57:55 +0300

    Added paths and fixed few names

--- products/getproducts.php
+++ products/getproducts.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
 
 $uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'),PHP_URL_PATH);
 $parameters = explode('/',$uri);
@@ -6,18 +8,17 @@ $prodcategory = $parameters[1];
 
 try {
     $db = openDb();
-    $sql = "select * from productgroup where prodcategory = $prodcategory";
+    $sql = "select * from productgroup where id = $prodcategory";
     $query = $db->query($sql);
-    $category = $query->fetch(PDO::FETCH_ASSOC);
+    $productgroup = $query->fetch(PDO::FETCH_ASSOC);
 
-    $db = openDb();
     $sql = "select * from product where prodcategory = $prodcategory";
     $query = $db->query($sql);
-    $category = $query->fetch(PDO::FETCH_ASSOC);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
 
     header('HTTP/1.1 200 OK');
     echo json_encode(array(
-        "category" => $category['prodname'],
+        "productgroup" => $productgroup['prodname'],
         "products" => $products
     ));
 }
