242522946fe75c8a9b9bfc8a1a0868089d8e1c96
Olli-Pekka <n1haol00@students.oamk.fi>
2022-04-08 15:06:07 +0300

    Added discproperty table

--- sql/kiekkokulma.sql
+++ sql/kiekkokulma.sql
@@ -16,19 +16,49 @@ name VARCHAR(100) NOT NULL,
 price DECIMAL(10,2),
 image VARCHAR(50),
 cost DECIMAL(10,2),
+manufacturer VARCHAR(30),
 prodcategory int NOT NULL,
 index prodcategory(prodcategory),
 foreign key (prodcategory) references productgroup(id)
 on delete restrict
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into product (name, price, prodcategory) values 
-('DX Valkyrie', 10.90, 1),
-('Star Boss', 18.90, 1),
-('DX Leopard', 10.90, 1),
-('DX Aero', 12.90, 1),
-('DX Archangel', 10.90, 1),
-('Champion Krait', 17.90, 1);
+CREATE TABLE discproperty (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    disc INTEGER NOT NULL,
+    type VARCHAR(20),
+    plastic VARCHAR(20),
+    speed INTEGER,
+    glide INTEGER,
+    turn INTEGER,
+    fade INTEGER,
+    foreign key (disc) references product(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+insert into product (name, price, manufacturer, prodcategory) values 
+('Innova DX Valkyrie', 'Innova'10.90, 1),
+('Innova Star Boss', 'Innova', 18.90, 1),
+('Innova DX Leopard', 'Innova', 10.90, 1),
+('Innova DX Aero', 'Innova', 12.90, 1),
+('Innova DX Aviar', 'Innova', 10.90, 1),
+('Innova DX Archangel', 'Innova', 10.90, 1),
+('Innova Champion Krait', 'Innova', 17.90, 1),
+('Innova Champion TeeBird', 'Innova', 20.90, 1),
+('Prodigy 400 D3', 'Prodigy', 17.90, 1),
+('Westside Discs BT Origio Pohjan Poika', 'Westside Discs', 10.90, 1);
+
+insert into discproperty (disc, type, plastic, speed, glide, turn, fade) values
+(1, 'Pituusdraiveri', 'DX', 9, 4, -2, 2),
+(2, 'Pituusdraiveri', 'Star', 13, 5, -1, 3),
+(3, 'Väylädraiveri', 'DX', 6, 5, -2, 1),
+(4, 'Putteri', 'DX', 3, 6, 0, 0),
+(5, 'Putteri', 'DX', 2, 3, 0, 1),
+(6, 'Pituusdraiveri', 'DX', 8, 6, -4, 1),
+(7, 'Pituusdraiveri', 'Champion', 11, 5, -1, 2),
+(8, 'Väylädraiveri', 'Champion', 7, 5, 0, 2),
+(9, 'Pituusdraiveri', '400', 13, 6, -2, 2),
+(10, 'Pituusdraiveri', 'BT Origio', 10, 5, -1, 2);
+
 insert into product (name, price, prodcategory) values
 ('DISCatcher Traveler', 149.00, 2),
 ('DISCatcher EZ Target', 199.00, 2);
