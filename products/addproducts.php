ccf0fc91f5af9b4c144aa3f1e2d88af5867a2774
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-04-19 20:20:26 +0300

    add products

---
+++ products/addproducts.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$name = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$price = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$image = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$manufacturer = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$prodcategory = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+try {
+    $db = openDb();
+    $sql = "insert into product (name, price, image, manufacturer, prodcategory) values ('$name', '$price', '$image', '$manufacturer', '$prodcategory')";
+    executeInsert($db,$sql);
+  $data = array('id' => $db->lastInsertId(), 'name' => $name, 'price' => $price, 'image' => $image, 'manufacturer' => $manufacturer, 'prodcategory' => $prodcategory);
+    print json_encode($data);
+   
+} catch(PDOException $pdoex) {
+    returnError($pdoex);
+}
