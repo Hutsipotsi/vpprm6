3f179b26ed4b27257a461c259be633438309a924
Annala <n1anje00@students.oamk.fi>
2022-04-19 11:11:29 +0300

    Renamed file again

--- products/getproductgroups.php
+++ products/getproductgroups.php
@@ -6,9 +6,6 @@ require_once '../inc/headers.php';
 // $parameters = explode('/',$uri);
 // $category = $parameters[1];
 
-
-
-
 try {
     $db = openDb();
     selectAsJson($db, 'select * from productgroup');
