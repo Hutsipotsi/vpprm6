be3c966cbf550a72a671d0fd60c7932ea32328af
Olli-Pekka <n1haol00@students.oamk.fi>
2022-05-01 17:58:42 +0300

    Login/logout changes/functionality

---
+++ user/checkuser.php
@@ -0,0 +1,48 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$email = filter_var($input->email, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$pw = filter_var($input->pw, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+//Tarkistetaan onko muttujia asetettu
+if (!isset($email) || !isset($pw)) {
+    throw new Exception("Pakollisia tietoja puuttuu, ei voi kirjautua.");
+}
+
+//Tarkistetaan, ettei tyhjiä arvoja muuttujissa
+if (empty($email) || empty($pw)) {
+    throw new Exception("Kirjautumistiedot eivät voi olla tyhjät.");
+}
+
+$pdo = openDb();
+
+try {
+    $pdo->beginTransaction();
+
+    //Haetaan käyttäjä annetulla käyttäjänimellä
+    $sql = "SELECT fname, email, password FROM registered_user WHERE email=?";
+    $statement = $pdo->prepare($sql);
+    $statement->bindParam(1, $email);
+    $statement->execute();
+    $pdo->commit();
+
+    if ($statement->rowCount() <= 0) {
+        throw new Exception("Käyttäjää ei löytynyt!");
+    }
+
+    $row = $statement->fetch();
+
+    //Tarkistetaan käyttäjän antama salasana tietokannan salasanaa vasten
+    if (!password_verify($pw, $row["password"])) {
+        throw new Exception("Väärä salasana!!");
+    }
+    else {
+        header('HTTP/1.1 200 OK');
+        echo json_encode(array('firstname' => $row["fname"]));
+    }
+} catch (PDOException $e) {
+    $pdo->rollback();
+    throw $e;
+}
---
+++ user/logout.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+session_unset();
+session_destroy();
+
+// EOF
